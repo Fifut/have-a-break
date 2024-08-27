613d211128299c53a3f859a7922e64472b4b3a6b
Fabien Streit <fabien.streit@gmail.com>
2024-08-27 12:42:51 +0200

    Base project with break time of 60 min

--- addons/have_a_break/have_a_break.gd
+++ addons/have_a_break/have_a_break.gd
@@ -1,19 +1,63 @@
+@tool
 extends Control
 
 
 @onready var time_left_label = %TimeLeftLabel
 @onready var break_time_popup = %BreakTimePopup
+@onready var start_button = %StartButton
+@onready var pause_button = %PauseButton
+@onready var reset_button = %ResetButton
+
+@onready var sound_check_box = %SoundCheckBox
+
 @onready var timer = %Timer
 @onready var audio_stream_player = %AudioStreamPlayer
 
 
+func _ready():
+	timer.paused = true
+	time_left_label.text = "Time left: " + _sec_to_string(timer.wait_time)
+
+
 func _process(delta):
-	time_left_label.text = timer.time_left
+	if not timer.is_stopped():
+		time_left_label.text = "Time left: " + _sec_to_string(timer.time_left)
+	else:
+		time_left_label.text = "Time left: " + _sec_to_string(timer.wait_time)
+	
+	start_button.disabled = not timer.paused
+	pause_button.disabled = timer.paused
+
+
+func _sec_to_string(sec: int):
+	var seconds = sec % 60
+	var minutes = (sec / 60) % 60
+	var hours = (sec / 60) / 60
+	return "%02d:%02d:%02d" % [hours, minutes, seconds]
 
 
 func _on_timer_timeout():
-	audio_stream_player.play()
+	if sound_check_box.button_pressed:
+		audio_stream_player.play()
+	break_time_popup.show()
 
 
 func _on_start_button_pressed():
-	timer.start()
+	timer.paused = false
+	if timer.time_left == 0:
+		timer.start()
+
+
+func _on_pause_button_pressed():
+	timer.paused = true
+
+
+func _on_reset_button_pressed():
+	timer.paused = true
+	timer.stop()
+
+
+func _on_stop_button_pressed():
+	audio_stream_player.stop()
+	break_time_popup.hide()
+	_on_reset_button_pressed()
--- addons/have_a_break/plugin.gd
+++ addons/have_a_break/plugin.gd
@@ -11,4 +11,4 @@ func _enter_tree():
 
 func _exit_tree():
 	remove_control_from_docks(_dock_instance)
-	_dock_instance.queue_free()
+	_dock_instance.free()
